c2a0f7d578e57ec60fde83cb2edbf072d7a27cf9
Mykael Barnes <mykael.barnes@hotmail.com>
2026-03-24 22:40:19 -0700

    feat(DbContext): OnModelCreating now includes SnippetTag and Favorite

--- backend/Data/AppDbContext.cs
+++ backend/Data/AppDbContext.cs
@@ -11,4 +11,19 @@ public class AppDbContext : DbContext
     }
 
     public DbSet<Snippet> Snippets => Set<Snippet>();
+    public DbSet<User> Users => Set<User>();
+    public DbSet<Tag> Tags => Set<Tag>();
+    public DbSet<SnippetTag> SnippetTags => Set<SnippetTag>();
+    public DbSet<Favorite> Favorites => Set<Favorite>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<SnippetTag>()
+            .HasKey(st => new { st.SnippetId, st.TagId });
+
+        modelBuilder.Entity<Favorite>()
+            .HasKey(f => new { f.UserId, f.SnippetId });
+    }
 }
